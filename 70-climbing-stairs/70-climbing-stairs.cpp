a293800c15d37c93774b8aa0b16e9625c1cbb267
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2022-03-13 22:30:52 +0530

    Time: 2 ms (31.96%), Space: 5.9 MB (54.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,22 +1,18 @@
 class Solution {
-   public:
-    
-    int climbStairs(int n) {
-    // base cases
-    if(n <= 0) return 0;
-    if(n == 1) return 1;
-    if(n == 2) return 2;
-    
-    int one_step_before = 2;
-    int two_steps_before = 1;
-    int all_ways = 0;
-    
-    for(int i=2; i<n; i++)
-    {
-    	all_ways = one_step_before + two_steps_before;
-    	two_steps_before = one_step_before;
-        one_step_before = all_ways;
-    }
-    return all_ways;
+public:
+   int climbStairs(int n) {
+        if(n<=2) return n;
+        int prev=2;
+        int prev2=1;
+        int res=0;
+        
+        for(int i=2; i<n; i++)
+        {
+            res = prev + prev2;
+            prev2 = prev;
+            prev = res;
+        }
+        
+        return res;
     }
 };
