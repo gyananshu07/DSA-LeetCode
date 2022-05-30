389544514c31a404e795dad0b5db09763db5aa0e
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2022-05-30 20:52:42 +0530

    Time: 70 ms (5.11%), Space: 18 MB (64.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         int sum = 0; // sum of all numbers initially 0
-        int total = nums.size()*(nums.size() + 1)/2; // total is the sum of all numbers in the array
-        for (auto number : nums) { // iterate through the array
-            sum += number; // add the number to the sum
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        int ans;
+        
+        for(int i = 0 ; i <= n ; i++)
+        {    
+            if(!binary_search(nums.begin(), nums.end(), i))
+            {
+                ans = i;
+                break;
+            }
         }
-        return total - sum; // return the difference between the sum and the total that is the missing number
+        
+        return ans;
     }
 };
