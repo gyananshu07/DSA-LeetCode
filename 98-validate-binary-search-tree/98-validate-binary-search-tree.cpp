82b2409dd38b2e521b7392abbe7e86e9aecdea05
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2022-10-04 15:16:34 +0530

    Time: 6 ms (98.72%), Space: 21.5 MB (98.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -15,7 +15,7 @@ public:
     bool solve(TreeNode* root, long long int min,long long int max){
         if(!root)return true;
         
-        if(root->val>=max || root->val<=min)return false;
+        if(root->val>=max || root->val<=min) return false;
         
         return solve(root->left,min,root->val) and solve(root->right,root->val,max);
     }
