65437e6bfaca3c5b8f634a66d3ed7e9b77b8a8f0
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2022-10-09 14:27:49 +0530

    Time: 277 ms (88.24%), Space: 16.4 MB (48.63%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,36 +1,37 @@
-class Solution {
-public:
-    int threeSumClosest(vector<int>& nums, int target) 
-    {
-        sort(nums.begin(),nums.end());
-        int N=nums.size();
-        int diff=INT_MAX,answer;
-        for(int i=0;i<N;i++)
+class Solution
+{
+    public:
+        int threeSumClosest(vector<int> &nums, int target)
         {
-            int low=i+1;
-            int high=N-1;
-            while(low<high)
+            sort(nums.begin(), nums.end());
+            int N = nums.size();
+            int diff = INT_MAX, answer;
+            for (int i = 0; i < N; i++)
             {
-                int sum=nums[i]+nums[low]+nums[high];
-                if(abs(sum-target)<diff)
+                int low = i + 1;
+                int high = N - 1;
+                while (low < high)
                 {
-                    diff=abs(sum-target);
-                    answer=sum;
-                }
-                if(sum==target)
-                {
-                    break;
-                }
-                else if(sum<target)
-                {
-                    low++;
-                }
-                else
-                {
-                    high--;
+                    int sum = nums[i] + nums[low] + nums[high];
+                    if (abs(sum - target) < diff)
+                    {
+                        diff = abs(sum - target);
+                        answer = sum;
+                    }
+                    if (sum == target)
+                    {
+                        break;
+                    }
+                    else if (sum < target)
+                    {
+                        low++;
+                    }
+                    else
+                    {
+                        high--;
+                    }
                 }
             }
+            return answer;
         }
-       return answer;
-    }
 };
