0a2f4cec1d3615caa5e6bcf63a743f7d81423f7b
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2023-02-06 22:32:34 +0530

    Time: 342 ms (24.04%), Space: 149.9 MB (5.11%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -5,40 +5,47 @@ private:
         int n = land.size();
         int m = land[0].size();
         
-        queue<pair<int, int>> q;
+        queue<pair<int, int>> q; // queue
         
         q.push({i, j});
         vis[i][j] = 1;
+        
+        //push starting coordinates of BFS of a connected component to temp
         temp.push_back(i);
         temp.push_back(j);
         
+        //4 Dir ->  R,  D, L, U
         int dr[] = {0, -1, 0, 1};
         int dc[] = {1, 0, -1, 0};
         
-        int lrow = 0, lcol = 0; 
+        int lrow = 0, lcol = 0; //stores the last row & col
         
         while(!q.empty())
         {
-            int row = q.front().first;
+            int row = q.front().first; 
             int col = q.front().second;
             q.pop();
             
             for(int i=0; i<4; i++)
             {
-                int nrow = row + dr[i];
-                int ncol = col + dc[i];
+                int nrow = row + dr[i]; //neighbour row
+                int ncol = col + dc[i]; //neighbour col
                 
                 if(nrow>=0 && ncol>=0 && nrow<n && ncol<m
-                  && !vis[nrow][ncol] && land[nrow][ncol] == 1)
+                  && !vis[nrow][ncol] && land[nrow][ncol] == 1) //check if the neigbouring row & col is in boundary & visited &                   that is a farmland
                 {
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1;  //mark visited
+                    q.push({nrow, ncol}); //push new row & col to queue
                     
                 }
             }
+            
+            //stores the last row & col
             lrow = row;
             lcol = col;            
         }
+        
+        //push ending coordinates of BFS of a connected component to temp
         temp.push_back(lrow);
         temp.push_back(lcol);
         
@@ -48,23 +55,22 @@ public:
         int n = land.size();
         int m = land[0].size();
         
-        vector<int> temp;
         vector<vector<int>> ans;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> vis(n, vector<int>(m, 0)); //visited array
         
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
                 vector<int> temp;
-                if(!vis[i][j] && land[i][j]==1)
+                if(!vis[i][j] && land[i][j]==1) //check if not visited and is a farmland
                 {
-                    bfs(i, j, vis, land, temp);
-                    ans.push_back(temp);
+                    bfs(i, j, vis, land, temp); // BFS Call
+                    ans.push_back(temp); // ans 2D array
                 }
             }
         }
         
-        return ans;
+        return ans; //returns the 2D array
     }
 };
