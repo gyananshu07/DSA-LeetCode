5785bad86664ba67d64dcc131d20c01fa8db3fea
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2023-02-10 01:45:26 +0530

    Time: 5 ms (75.61%), Space: 8.7 MB (64.39%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,52 +1,28 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
-    public:
-    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-            for(auto it:cur->neighbors)
-            {
-                if(mp.find(it)!=mp.end())   //already clone and stored in map
-                {
-                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
-                }
-                else
-                    neighbour.push_back(dfs(it,mp));
-            }
-            clone->neighbors=neighbour;
-            return clone;
-    }
+public:
+    unordered_map<Node* , Node*> mp; // declaring map, to check whwther we have a copy of node or not and also to store copy
+    
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*> mp;
-        if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0)   //if only one node present no neighbors
+        if(node == NULL) // if node is null, then simply return null
         {
-            Node* clone= new Node(node->val);
-            return clone; 
+            return NULL;
         }
-        return dfs(node,mp);
+        
+        // for a node, we will check whether we already creates a copy of thiis or not. If it is present in map that means we already creates a copy of this.
+        //But if not present in map, that means we have not a copy of this.
+        // Also, if we create a copy, then being a good neighbor, we find whether our neighbor have a copy or not, so we will travel all around our adjcant.
+        
+        if(mp.find(node) == mp.end()) // if not present in map
+        {
+            mp[node] = new Node(node -> val, {}); // make a copy
+            
+            for(auto adj: node -> neighbors) // travel in adjcant
+            {
+                mp[node] -> neighbors.push_back(cloneGraph(adj)); //add copy
+            }
+        }
+        
+        return mp[node]; // and at last, return mp[node] as till now we clone our whole graph
+        
     }
 };
