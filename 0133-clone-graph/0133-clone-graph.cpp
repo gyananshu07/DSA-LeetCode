324658d04fcb5e5849640efb60e908afe4427375
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2023-02-10 01:46:28 +0530

    Time: 7 ms (70.40%), Space: 8.6 MB (87.35%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -8,21 +8,30 @@ public:
             return NULL;
         }
         
-        // for a node, we will check whether we already creates a copy of thiis or not. If it is present in map that means we already creates a copy of this.
-        //But if not present in map, that means we have not a copy of this.
-        // Also, if we create a copy, then being a good neighbor, we find whether our neighbor have a copy or not, so we will travel all around our adjcant.
+        Node* first = new Node(node -> val, {}); // make a copy of first node
+        mp[node] = first;
         
-        if(mp.find(node) == mp.end()) // if not present in map
+        queue<Node*> q; //For bfs, we create queue
+        q.push(node); // push into queue
+        
+        while(q.empty() == false) // until q. empty == false
         {
-            mp[node] = new Node(node -> val, {}); // make a copy
+            Node* curr = q.front(); // extract front node
+            q.pop(); // pop that from queue
             
-            for(auto adj: node -> neighbors) // travel in adjcant
+            for(auto adj: curr -> neighbors) // now travel in adjcant
             {
-                mp[node] -> neighbors.push_back(cloneGraph(adj)); //add copy
+                if(mp.find(adj) == mp.end()) // if not present in map
+                {
+                    mp[adj] = new Node(adj -> val, {}); // then create copy
+                    q.push(adj); // push nto the queue
+                    
+                }
+                
+                mp[curr] -> neighbors.push_back(mp[adj]); // in current node push adjcant node
             }
         }
         
-        return mp[node]; // and at last, return mp[node] as till now we clone our whole graph
-        
+        return mp[node];
     }
 };
