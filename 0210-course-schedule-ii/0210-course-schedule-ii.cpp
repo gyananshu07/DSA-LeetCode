8eadc76dd89da2735269f4227b5770cec0c6ef43
Gyananshu Kashyap <81867317+gyananshu07@users.noreply.github.com>
2023-02-11 00:12:56 +0530

    Time: 26 ms (56.93%), Space: 13 MB (96.53%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
+        
+        vector<int> adj[V];
+        vector<int> vis(V, 0), topo, indegree(V, 0);
+	    queue<int> q;
+        
+        for(int i=0; i<prerequisites.size(); i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+	    
+	    for(int i=0; i<V; i++)
+	    {
+	        for(auto it : adj[i])
+	        {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i=0; i<V; i++)
+	    {
+	        if(indegree[i] == 0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        
+	        topo.push_back(node);
+	        
+	        for(auto it : adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it] == 0)
+	            {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    
+        vector<int> ans;
+        reverse(topo.begin(), topo.end());
+	    return topo.size() == V ? topo : ans;
+    }
+};
